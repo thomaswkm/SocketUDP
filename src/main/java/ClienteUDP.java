a821f0ffd9f8baa6b77849abde295f1e897027a8
thomas <t.kinderman01@ufromail.cl>
2023-07-02 05:48:24 -0400

    Commit

--- src/main/java/ClienteUDP.java
+++ src/main/java/ClienteUDP.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class ClienteUDP {
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class ClienteUDP {
+
+    public static void main(String[] args) {
+
+        try {
+            InetAddress ipServidor = InetAddress.getByName("localhost");
+            DatagramSocket socketUdp = new DatagramSocket();
+            System.out.println("Ingresa las coordenadas del pixel, separadas por una coma");
+            String coordenadas = new Scanner(System.in).nextLine();
+            byte[] bufferEnvio = coordenadas.getBytes();
+            byte[] bufferRecepcion = new byte[10000];
+
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(200, 200);
+            JPanel panel = new JPanel();
+            frame.add(panel);
+            frame.setVisible(true);
+
+            while (true) {
+                DatagramPacket paqueteSaliente = new DatagramPacket(bufferEnvio, bufferEnvio.length, ipServidor, 8888);
+                socketUdp.send(paqueteSaliente);
+
+                DatagramPacket paqueteEntrante = new DatagramPacket(bufferRecepcion, bufferRecepcion.length);
+                socketUdp.receive(paqueteEntrante);
+
+                String pixelInfo = new String(paqueteEntrante.getData(), paqueteEntrante.getOffset(), paqueteEntrante.getLength());
+                System.out.println(pixelInfo);
+
+
+                String hexValue = pixelInfo.substring(pixelInfo.lastIndexOf("0x") + 2);
+
+                int pixelValue = (int) Long.parseLong(hexValue, 16);
+
+                Color color = new Color(pixelValue);
+
+                panel.setBackground(color);
+            }
+
+        } catch (UnknownHostException e) {
+            System.out.println("Error al ingresar la IP del cliente");
+        } catch (SocketException e) {
+            System.out.println("Error al crear el socket");
+        } catch (IOException e) {
+            System.out.println("Error al enviar o recibir un paquete");
+        }
+    }
 }
--- src/main/java/ServerUDP.java
+++ src/main/java/ServerUDP.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class ServerUDP {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class ServerUDP {
+
+    public static void main(String[] args) {
+
+        try {
+            Robot robot = new Robot();
+            DatagramSocket socketUDP = new DatagramSocket(8888);
+            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+
+            while (true) {
+                byte[] buffer = new byte[10000];
+                DatagramPacket paqueteEntrante = new DatagramPacket(buffer, buffer.length);
+                socketUDP.receive(paqueteEntrante);
+                String coordenadas = new String(paqueteEntrante.getData(), 0, paqueteEntrante.getLength());
+
+                BufferedImage screenshot = robot.createScreenCapture(screenRect);
+
+                int x = Integer.parseInt(coordenadas.split(",")[0].trim());
+                int y = Integer.parseInt(coordenadas.split(",")[1].trim());
+
+                int pixelValue = screenshot.getRGB(x, y);
+
+                String pixelInfo = String.format("Pixel en (%d, %d): 0x%08X", x, y, pixelValue);
+                buffer = pixelInfo.getBytes();
+
+                int puertoCliente = paqueteEntrante.getPort();
+                InetAddress ipCliente = paqueteEntrante.getAddress();
+
+                DatagramPacket paqueteSaliente = new DatagramPacket(buffer, buffer.length, ipCliente, puertoCliente);
+                socketUDP.send(paqueteSaliente);
+            }
+
+        } catch (SocketException e) {
+            System.out.println("Error al crear el socket");
+        } catch (IOException e) {
+            System.out.println("Error al recibir o enviar un paquete");
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
